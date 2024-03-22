f5c1e4859d7ee5c2b768cb0cb77821ff8f03f6d7
vishGurav <142220564+vishGurav@users.noreply.github.com>
2024-03-22 06:02:42 -0700

    Add files via upload

---
+++ website4/app.js
@@ -0,0 +1,9 @@
+let iframe=document.createElement("iframe")
+let a=document.createAttribute("src") 
+a=""
+let div=document.getElementsByClassName("c1")
+function html()
+{
+    // document.getElementById("ff").innerHTML="<iframe src="demo.txt" style="height: 500px;"></iframe>"
+    divv.append(iframe)
+}
